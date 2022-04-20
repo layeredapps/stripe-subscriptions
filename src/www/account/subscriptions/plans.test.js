fa7ebea984eaa731d675dfcd91f6dc8826dacb56
beninsydney <benodev@protonmail.com>
2022-04-21 06:30:47 +1000

    Fix page size

--- src/www/account/subscriptions/plans.test.js
+++ src/www/account/subscriptions/plans.test.js
@@ -43,10 +43,10 @@ describe('/account/subscriptions/plans', function () {
     ]
     await req1.route.api.before(req1)
     cachedResponses.before = req1.data
+    global.pageSize = 50
     cachedResponses.returns = await req1.get()
     global.pageSize = 3
     cachedResponses.pageSize = await req1.get()
-    global.pageSize = 50
     const req2 = TestHelper.createRequest('/account/subscriptions/plans?offset=1')
     req2.account = user.account
     req2.session = user.session
@@ -65,24 +65,29 @@ describe('/account/subscriptions/plans', function () {
   describe('view', () => {
     it('should return one page (screenshots)', async function () {
       await bundledData(this.test.currentRetry())
+      global.pageSize = 50
       const result = cachedResponses.returns
+      const doc = TestHelper.extractDoc(result.html)
+      const table = doc.getElementById('plans-table')
+      const rows = table.getElementsByTagName('tr')
+      assert.strictEqual(rows.length, 5)
+      // 4 created in loop
+      // table heading
+    })
+
+    it('should change page size', async function () {
+      await bundledData(this.test.currentRetry())
+      global.pageSize = 3
+      const result = cachedResponses.pageSize
       const doc = TestHelper.extractDoc(result.html)
       const table = doc.getElementById('plans-table')
       const rows = table.getElementsByTagName('tr')
       assert.strictEqual(rows.length, global.pageSize + 1)
     })
 
-    it('should change page size', async function () {
-      await bundledData(this.test.currentRetry())
-      const result = cachedResponses.pageSize
-      const doc = TestHelper.extractDoc(result.html)
-      const table = doc.getElementById('plans-table')
-      const rows = table.getElementsByTagName('tr')
-      assert.strictEqual(rows.length, global.pageSize + 2)
-    })
-
     it('should change offset', async function () {
       await bundledData(this.test.currentRetry())
+      global.pageSize = 2
       const offset = 1
       const result = cachedResponses.offset
       const doc = TestHelper.extractDoc(result.html)
