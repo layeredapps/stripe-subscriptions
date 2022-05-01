98e8d2ef464d7bcebe11a06a574a9dfdbc08e2c9
beninsydney <benodev@protonmail.com>
2022-05-01 15:56:22 +1000

    Check for uuid-ossp

--- src/storage-postgresql.js
+++ src/storage-postgresql.js
@@ -14,5 +14,6 @@ module.exports = async () => {
       idle: process.env.SUBSCRIPTIONS_IDLE_CONNECTION_LIMIT || process.env.IDLE_CONNECTION_LIMIT || 10000
     }
   })
+  await sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
   return sequelize
 }
