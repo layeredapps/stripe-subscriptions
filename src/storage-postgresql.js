4192acd5ad703f60a77032f99ba513290c0e6223
beninsydney <benodev@protonmail.com>
2022-05-02 18:25:35 +1000

    fix: postgres ssl

--- src/storage-postgresql.js
+++ src/storage-postgresql.js
@@ -1,17 +1,30 @@
 const { Sequelize } = require('sequelize')
-const Log = require('@layeredapps/dashboard/src/log.js')('sequelize-stripe-subscriptions-postgresql')
+const Log = require('@layeredapps/dashboard/src/log.js')('sequelize-stripe-connect-postgresql')
 
 module.exports = async () => {
-  const prefixedDatabaseURL = process.env.SUBSCRIPTIONS_POSTGRESQL_DATABASE_URL || process.env.POSTGRESQL_DATABASE_URL
-  const sequelize = new Sequelize(prefixedDatabaseURL, {
-    dialect: 'sqlite',
-    logging: (sql) => {
-      return Log.info(sql)
-    },
+  let url = process.env.SUBSCRIPTIONS_POSTGRESQL_DATABASE_URL || process.env.POSTGRESQL_DATABASE_URL
+  const sslModeRequiredIndex = url.indexOf('?sslmode=require')
+  let dialectOptions
+  if (sslModeRequiredIndex > -1) {
+    url = url.substring(0, sslModeRequiredIndex)
+    dialectOptions = {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      keepAlive: true
+    }
+  }
+  const sequelize = new Sequelize(url, {
+    dialect: 'postgres',
+    dialectOptions,
     pool: {
       max: process.env.SUBSCRIPTIONS_MAX_CONNECTIONS || process.env.MAX_CONNECTIONS || 10,
       min: 0,
       idle: process.env.SUBSCRIPTIONS_IDLE_CONNECTION_LIMIT || process.env.IDLE_CONNECTION_LIMIT || 10000
+    },
+    logging: (sql) => {
+      return Log.info(sql)
     }
   })
   return sequelize
