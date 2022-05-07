3df720e1fb4de27dbfeeabc8aeb23bc5d7b9d69b
beninsydney <benodev@protonmail.com>
2022-05-07 11:54:16 +1000

    Use global.stripeKey

--- src/server/bind-stripekey.js
+++ src/server/bind-stripekey.js
@@ -4,6 +4,6 @@ module.exports = {
 
 async function bindStripeKey (req) {
   req.stripeKey = req.stripeKey || {
-    apiKey: process.env.SUBSCRIPTIONS_STRIPE_KEY || process.env.STRIPE_KEY
+    apiKey: global.stripeKey
   }
 }
