a6c214751b0e6eaa70524760eaad8a17f9ed4a49
beninsydney <benodev@protonmail.com>
2022-05-24 12:14:34 +1000

    fix: optional link removal

--- src/www/account/subscriptions/navbar.js
+++ src/www/account/subscriptions/navbar.js
@@ -1,9 +1,10 @@
 module.exports = {
   setup: (doc) => {
     if (global.viewSubscriptionPlans === false) {
-      const startLink = doc.getElementById('navbar-start-link')
+      const template = doc.getElementById('navbar')
+      const startLink = template.getElementById('navbar-start-link')
       startLink.parentNode.removeChild(startLink)
-      const plansLink = doc.getElementById('navbar-plans-link')
+      const plansLink = template.getElementById('navbar-plans-link')
       plansLink.parentNode.removeChild(plansLink)
     }
   }
